686657a71eacf8234311a500bb38aea9d1d1cc66
Ahmed Memon <ahmedmemon12644@gmail.com>
2024-08-19 00:13:39 +0500

    push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Navbar = document.querySelector('nav')
 const Logo = document.getElementById('Logo')
+const navMenu = document.querySelector('.menu-btn')
 
 window.addEventListener('scroll', function () {
     const currentPostion = window.pageYOffset
@@ -7,15 +8,23 @@ window.addEventListener('scroll', function () {
     if (currentPostion >= 600) {
       Navbar.classList.add('Nav')
       Logo.classList.add('OnScroll')
+      navMenu.classList.add('NavBtn')
     }
     else if (currentPostion < 600) {
       Logo.classList.remove('OnScroll')
       Navbar.classList.remove('Nav')
+      navMenu.classList.remove('NavBtn')
     }
   })
-  
+  document.addEventListener('DOMContentLoaded', function() {
+    // Add 'loaded' class to body when page is fully loaded
+    document.body.classList.add('loaded');
+});
+
   $(".hover").mouseleave(
     function () {
       $(this).removeClass("hover");
     }
   );
+
+
